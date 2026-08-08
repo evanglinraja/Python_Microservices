69ac86549d49f07a336935ba511e83bb7423d217
evanglinraja <evanmary1901@gmail.com>
2026-08-08 15:11:25 +0530

    Mask database password in database.py
    Replaced sensitive database password with a placeholder.

--- Services/orders/core/database.py
+++ Services/orders/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:Evan%402001@localhost:3306/orderdb"
+DATABASE_URL = "mysql+pymysql://root:******@localhost:3306/orderdb"
 
 engine= create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
