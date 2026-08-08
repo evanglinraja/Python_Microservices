cb9fe7c9fd85f785a291e8df7227390871c97b06
evanglinraja <evanmary1901@gmail.com>
2026-08-08 15:10:20 +0530

    Obscure database password in connection string
    Obscured the database password in the DATABASE_URL.

--- Services/products/core/database.py
+++ Services/products/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:Evan%402001@localhost:3306/productdb"
+DATABASE_URL = "mysql+pymysql://root:******@localhost:3306/productdb"
 
 engine= create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
